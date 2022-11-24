63457409ee13e34a1f7faf3ebe474108823a5d0e
lamiro3 <lamiro3@naver.com>
2022-11-24 12:45:44 +0900

    facial_landmark with mask

--- Face_RM_maker.py
+++ Face_RM_maker.py
@@ -5,29 +5,32 @@ import numpy as np
 import os
 import glob
 
-base_dir = './IMG/train/'
-train_data, train_labels = [], []
-recognizer = cv2.face.LBPHFaceRecognizer_create()
+def RMmaker(base_dir, name):
+    train_data, train_labels = [], []
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-dirs = [d for d in glob.glob(base_dir+'/*') if os.path.isdir(d)]
+    dirs = [d for d in glob.glob(base_dir+'/*') if os.path.isdir(d)]
+    print(dirs)
+    print('Collecting train data set...')
 
-print('Collecting train data set...')
-
-for dir in dirs:
-    id = dir.split('_')[1]
-    files = glob.glob(dir+'/*.jpg')
-    print('\t path:%s, %dfiles'%(dir, len(files)))
-    
-    for file in files:
-        img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-        train_data.append(np.asarray(img, dtype=np.uint8))
-        train_labels.append(int(id))
+    for dir in dirs:
+        id = int(dir.split('/')[4].split('\\')[1].split('_')[1])
+        files = glob.glob(dir+'/*.jpg')
+        print('\t path:%s, %dfiles'%(dir, len(files)))
         
-train_data = np.asarray(train_data)
-train_labels = np.int32(train_labels)
+        for file in files:
+            img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+            train_data.append(np.asarray(img, dtype=np.uint8))
+            train_labels.append(int(id))
+            
+    train_data = np.asarray(train_data)
+    train_labels = np.int32(train_labels)
 
-print('Starting LBP Model training...')
-model = recognizer
-model.train(train_data, train_labels)
-model.write('./IMG/train/all_face.xml')
-print("Model trained successfully!")
+    print('Starting LBP Model training...')
+    model = recognizer
+    model.train(train_data, train_labels)
+    model.write('C:/HORUS/Face_RM/Model/'+name+'_LBP_model.xml')
+    print("Model trained successfully!")
+
+RMmaker('C:/HORUS/Face_RM/IMG/masked', 'masked')
+RMmaker('C:/HORUS/Face_RM/IMG/original', 'original')
--- Face_recognition.py
+++ Face_recognition.py
@@ -1,16 +1,17 @@
 import cv2
-import numpy as np
 import math
 
 # id_ 수정하기 (우리 dataset id랑 체계가 다름)
 
-labels = ["s1", "s2", "s3", "Ko Taehyeon"]
+labels = ["kth", "ang"]
 
 base_dir = './IMG/train/'
 
 face_classifier = cv2.CascadeClassifier('C:\HORUS\Face_RM\Cascades\haarcascade_frontalface_default.xml')
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('C:\HORUS\Face_RM\model_2022-09-27 01_36_33.098850.xml')
+original_recognizer = cv2.face.LBPHFaceRecognizer_create()
+masked_recognizer = cv2.face.LBPHFaceRecognizer_create()
+original_recognizer.read('C:\HORUS\Face_RM\Model\original_LBP_model.xml')
+masked_recognizer.read('C:\HORUS\Face_RM\Model\masked_LBP_model.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -24,14 +25,20 @@ while True:
     
     for (x, y, w, h) in faces :
         ROI_gray = gray[y:y+h, x:x+w] # 앞 과정에서 get한 img에서 얼굴 부분만 따로 get
-        id_, conf = recognizer.predict(ROI_gray) # 얼굴 유사도 확인
-        print(id_, conf)
+        id_origin, conf_origin = original_recognizer.predict(ROI_gray) # 얼굴 유사도 확인
+        id_masked, conf_masked = masked_recognizer.predict(ROI_gray)
+        print(id_origin, conf_origin, conf_masked)
         
-        if conf>=50:
+        if conf_origin>50 and conf_origin > conf_masked:
             font = cv2.FONT_HERSHEY_SIMPLEX
-            name = labels[id_-1]
-            cv2.putText(img, f'{name}, coef:{math.ceil(conf)}%', (x,y), font, 1, (0, 0, 255), 2)
+            name = labels[id_origin-1]
+            cv2.putText(img, f'{name}, coef:{math.ceil(conf_origin)}%', (x,y), font, 1, (0, 0, 255), 2)
             cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), 2)
+        elif conf_masked>50 and conf_masked > conf_origin:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_masked-1]
+            cv2.putText(img, f'{name}, coef:{math.ceil(conf_masked)}%', (x,y), font, 1, (0, 0, 255), 2)
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
         
     cv2.imshow('Sample', img)
     if cv2.waitKey(10) >= 0:
